834930f7f9294cf19aa86496c87362b6577d5623
Gaganfnr <mangalgagan4@gmail.com>
2023-08-23 16:15:22 +0530

    ...

--- Project 14/test.js
+++ Project 14/test.js
@@ -1,4 +1,88 @@
-console.log("Gagan")
-let a = 10
-let b = 20
-console.log(a+b)
+// console.log("Gagan")
+// let a = 10
+// let b = 10
+// if(a==b)
+// {
+//     console.log(a-b)
+
+// }
+// else{
+//     console.log(a+b)
+// }
+
+// let number = 0;
+// console.log(number++);
+// console.log(++number);
+// console.log(number);
+
+// let a = 3
+// let b = new Number(3)
+// let c = 3
+// console.log(a == b);
+// console.log(a === b);
+// console.log(b === c);
+
+// let greeting;
+// greeting = {};
+// console.log(greeting);
+
+// const sum = eval("10*10+5");
+// console.log(sum);
+
+// var num = 8;
+// var num = 10;
+// console.log(num);
+
+// console.log(typeof typeof 1);
+
+// const number = [1, 2, 3];
+// number[10] = 11;
+// console.log(number);
+
+// console.log(3 + 4 + "5");
+
+// console.log("I enrolled to the PW FSWD course"[0]);
+
+// const name = "PW Skills";
+// console.log(name());
+
+// const one = false || {} || null;
+// const two = null  || false || "";
+// const three = [] || 0 || true;
+// console.log(one, two, three);
+
+// var x = 2;
+// var y = "2";
+// console.log(x === y);
+
+// for (let i = 1; i < 5; i++) {
+//   if (i == 3) continue;
+//   console.log(i);
+// }
+
+// let a = 2;
+// let b = 0;
+// let s;
+// s = a/b;
+// console.log(s);
+
+// const name = "PW Skills";
+// console.log(!typeof name == "object");
+// console.log(!typeof name == "string");
+
+// for (var i = 2; i < 5; i++)
+// {
+//     if (i === 3){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// var a = [1, 2, 3, 4];
+// for (var i in a) {
+//     console.log(a[i]);
+//   if (a[i] % 2 === 0) {
+//     // console.log(a[i]);
+//   }
+// }
+
