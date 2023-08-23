f86aeb02851e650fcec42fde754b2d8c07c18dca
Gaganfnr <mangalgagan4@gmail.com>
2023-08-23 23:06:44 +0530

    ...

---
+++ Project 15/Array.js
@@ -0,0 +1,39 @@
+let aa = ['cddc','ccdc','dccc']
+let cc = aa.length
+console.log(typeof aa);
+// a.push('kkkk')
+// // console.log(a);
+// a.pop()
+// // console.log(a);
+// a.unshift('ssss')
+// console.log(a);
+
+let a = "Hi, I am Gagan Mangal";
+// console.log("a",a,typeof(a));
+
+let b = 1;
+// console.log("b",b,typeof(b));
+
+let c = false;
+// console.log("c",c,typeof(c));
+
+let d = 0;
+// console.log("d",d,typeof(d));
+
+let f = Symbol("Key_1")
+
+let e = {
+    name: 'Gagan',
+    age: 20,
+    [f]: "Key_2"
+}
+// console.log(e,typeof(e));
+// console.table ([a,b,c,d,e])
+
+let r = 141014n
+// console.log("r",r,typeof(r));
+
+
+
+let k;
+// console.log("k",k,typeof(k));
