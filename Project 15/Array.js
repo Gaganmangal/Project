403d09d4b1e273cd85f87b2f75d539513aa06dce
Gaganfnr <mangalgagan4@gmail.com>
2023-08-24 11:27:03 +0530

    ...

--- Project 15/Array.js
+++ Project 15/Array.js
@@ -1,6 +1,6 @@
-let aa = ['cddc','ccdc','dccc']
-let cc = aa.length
-console.log(typeof aa);
+// let aa = ['cddc','ccdc','dccc']
+// let cc = aa.length
+// console.log(typeof aa);
 // a.push('kkkk')
 // // console.log(a);
 // a.pop()
@@ -20,20 +20,39 @@ let c = false;
 let d = 0;
 // console.log("d",d,typeof(d));
 
-let f = Symbol("Key_1")
+// let f = Symbol("Key_1");
 
-let e = {
-    name: 'Gagan',
-    age: 20,
-    [f]: "Key_2"
-}
+// let e = {
+//   name: "Gagan",
+//   age: 20,
+//   [f]: "Key_2",
+// };
 // console.log(e,typeof(e));
 // console.table ([a,b,c,d,e])
 
-let r = 141014n
+let r = 141014n;
 // console.log("r",r,typeof(r));
-
-
 
 let k;
 // console.log("k",k,typeof(k));
+
+// const first_name = 'Gagan'
+// const last_name = 'Mangal'
+// console.log(`Hi,My Name is ${first_name} ${last_name}`);
+
+const Name = new String("Deven")
+// console.log(Name.indexOf(1));
+// console.log(`Hi,My Name is ${Name}`);
+
+// const tagline = "MDN - Resources for developers, by developers";
+// console.log(tagline.indexOf("developers"));
+
+// const number = 1000
+// console.log(number);
+// const Number_7 = new Number(1800)
+// console.log(Number_7.indexOf(1));
+
+
+const newDate = new Date()
+// console.log(newDate.toLocaleDateString();
+// console.log(newDate.getTime());
