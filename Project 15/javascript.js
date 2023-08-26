d0e7a26d5da3db3c56add1ad73c77e77e56671d5
Gaganfnr <mangalgagan4@gmail.com>
2023-08-26 23:20:25 +0530

    ...

--- Project 15/javascript.js
+++ Project 15/javascript.js
@@ -111,5 +111,170 @@ let number = 6;
 // Write a program to perform all the arithmetic operations[except increment and decrement operators] of
 // javascript of any two numbers stored in the variables num1 and num2. Also, print the results to the console.
 
-let num1 = 10;
-let num2 = 8;
+// let num1 = 8;
+// let num2 = 8;
+
+// console.log(`The addition of num1 and num2 is ${num1 + num2}`);
+// console.log(`The substraction of num1 and num2 is ${num1 - num2}`);
+// console.log(`The multiplication of num1 and num2 is ${num1 * num2}`);
+// console.log(`The division of num1 and num2 is ${num1 / num2}`);
+// console.log(`The modulo of num1 and num2 is ${num1 % num2}`);
+// console.log(`The exponential of num1 and num2 is ${num1 ** num2}`);
+
+// 3. Write a program to find out the perimeter of a rectangle. Print the results to the console.
+
+let length = 10;
+let width = 20;
+let perimeterofRectangle = 2 * (length + width);
+
+// console.log(
+//   `The perimeter of the rectangle with length: ${length} and Width: ${width} is ${perimeterofRectangle}`
+// );
+
+// Write a program to demonstrate the results of comparison operators. Note that both the truth and false
+// condition for each operator must be specified.
+
+// let num1 = 12;
+// let num2 = 12;
+
+// console.log(num1 == num2);
+
+// let num3 = 12;
+// let num4 = 10;
+
+// console.log(num3 == num4);
+
+// let num11 = 12;
+// let num12 = 12;
+
+// console.log(num11 != num12);
+
+// let num13 = 12;
+// let num14 = 10;
+
+// console.log(num13 != num14);
+
+// let num21 = 12;
+// let num22 = 12;
+
+// console.log(num21 === num22);
+
+// let num23 = 12;
+// let num24 = "12";
+
+// console.log(num23 === num24);
+
+// let num31 = 12;
+// let num32 = 12;
+
+// console.log(num31 !== num32);
+
+// let num33 = 12
+// let num34 = "12"
+
+// console.log(num33 !== num34);
+
+// let num41 = 13;
+// let num42 = 12
+
+// console.log(num41 > num42);
+
+// let num43 = 10
+// let num44 = 12
+
+// console.log(num43 > num44);
+
+// let num51 = 13
+// let num52 = 12
+
+// console.log(num51 >= num52);
+
+// let num53 = 10
+// let num54 = 12
+
+// console.log(num53 >= num54);
+
+// let num61 = 12
+// let num62 = 13
+
+// console.log(num61 < num62);
+
+// let num63 = 12
+// let num64 = 10
+
+// console.log(num63 < num64);
+
+// let num71 = 13
+// let num72 = 13
+
+// console.log(num71 <= num72);
+
+// let num73 = 12
+// let num74 = 10
+
+// console.log(num73 <= num74);
+
+// Write a program of traffic control that accepts the traffic light displayed and prints the message. If the
+// traffic light is red print the vehicles must stop.
+
+// let trafficlight = "orange";
+// if (trafficlight == "red") {
+//   console.log("Vehicles must stop.");
+// } else if (trafficlight == "orange") {
+//   console.log("Vehicles must wait. The signal is changing to red or green.");
+// } else if (trafficlight == "green") {
+//   console.log("Vehicles may proceed with caution.");
+// } else {
+//   console.log("Invalid traffic light");
+// }
+
+// 2. Write a program to print the largest of 2 numbers.
+
+// let num1 = 20
+// let num2 = 15
+// if (num1 > num2) {
+//   console.log("num1 is greater than num2");
+// } else {
+//   console.log("num2 is greater than num1");
+// }
+
+// 3. Write a program that takes a number as input and outputs "Fizz" if it is divisible by 3, "Buzz" if it is divisible
+// by 5, and "FizzBuzz" if it is divisible by both 3 and 5. Note that any number can be passed and not restricted to
+// the numbers divisible by 3 or 5.
+
+// let num = 5;
+
+// if (num % 5 == 0 && num % 3 == 0){
+//   console.log("FizzBuzz");
+// } else if (num % 3 == 0){
+//   console.log("Fizz");
+// } else if (num % 5 == 0){
+//   console.log("Buzz");
+// } else {
+//   console.log("Invalid input");
+// }
+
+const day = "Tuesday";
+
+let daysUntilWeekend;
+
+switch (day) {
+  case "Monday":
+    console.log(`The are 5 day(s) until the weekend`);
+    break;
+  case "Tuesday":
+    console.log(`The are 5 day(s) until the weekend`);
+    break;
+  case "Monday":
+    console.log(`The are 5 day(s) until the weekend`);
+    break;
+  case "Monday":
+    console.log(`The are 5 day(s) until the weekend`);
+    break;
+  case "Monday":
+    console.log(`The are 5 day(s) until the weekend`);
+    break;
+  case "Monday":
+    console.log(`The are 5 day(s) until the weekend`);
+    break;
+}
