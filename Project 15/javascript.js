7665bc4c373805f4dfc782ab54b26b9fd2da1d4a
Gaganfnr <mangalgagan4@gmail.com>
2023-08-26 15:41:42 +0530

    ...

---
+++ Project 15/javascript.js
@@ -0,0 +1,115 @@
+const a = "Gagan Mangal";
+// console.log(`a = ${a} type of ${typeof a}`);
+
+const b = 1002;
+// console.log(`b = ${b} type of ${typeof b}`);
+
+const bb = 100.2;
+// console.log(`bb = ${bb} type of ${typeof bb}`);
+
+const bbb = Number.POSITIVE_INFINITY;
+// console.log(`bbb = ${bbb} type of ${typeof bbb}`);
+
+const bbbb = NaN;
+// console.log(`bbbb = ${bbbb} type of ${typeof bbbb}`);
+
+const c = 10025205244522n;
+// console.log(`c = ${c} type of ${typeof c}`);
+
+const d = false;
+// console.log(`d = ${d} type of ${typeof d}`);
+
+let e;
+// console.log(`e = ${e} type of ${typeof e}`);
+
+const f = null;
+// console.log(`f = ${f} type of ${typeof f}`);
+
+const key = Symbol("key_1");
+// console.log(key);
+
+const user = {
+  name: "Gagan Mangal",
+  email: "H@gamil.com",
+  age: 40,
+  loging: false,
+  [key]: "Key_2",
+};
+// console.log(user);
+
+const first_user = [
+  {
+    first_name: "Rohit",
+    Last_name: "Jain",
+  },
+  {
+    age: 45,
+    id: "12A",
+  },
+];
+// console.log(first_user);
+
+/*************************************************ARRAY**************************************************************/
+
+const ARRAY = [
+  "iphone",
+  "Macbook Pro",
+  "Flower Pot",
+  "Water Bottle",
+  "Mac Studio mini",
+  "Watch",
+  "Tennis Ball",
+  "Mouse Pad",
+  "Keyboard",
+  "Lens",
+];
+// console.log(`ARRAY ${ARRAY} type of ${typeof ARRAY}`);
+
+/*************************************************OBJECT**************************************************************/
+
+const object_1 = {
+  1: "Mithun",
+  2: "Alka",
+  3: "Anurag",
+  4: "Prabir",
+  5: "Shivam",
+};
+// console.log(`object_1 ${object_1} type of ${typeof object_1}`);
+// console.log(object_1);
+
+/*************************************************Create 2 Create 2 valid variables and 2 invalid variables and 2 invalid variables**************************************************************/
+
+// valid variables
+
+let name = "PW Skills";
+// console.log(name);
+let iAmHappy = true;
+// console.log(iAmHappy);
+
+// invalid variables
+
+// let 1name = "PW Skills"
+// console.log(1name);
+// let var = 13
+// console.log(var);
+
+// Operators
+
+// Write a program that prints the multiplication table in the textbook format of any number specified.
+let number = 6;
+// console.log(`${number} * 1 = ${number * 1}`);
+// console.log(`${number} * 2 = ${number * 2}`);
+// console.log(`${number} * 3 = ${number * 3}`);
+// console.log(`${number} * 4 = ${number * 4}`);
+// console.log(`${number} * 5 = ${number * 5}`);
+// console.log(`${number} * 6 = ${number * 6}`);
+// console.log(`${number} * 7 = ${number * 7}`);
+// console.log(`${number} * 8 = ${number * 8}`);
+// console.log(`${number} * 9 = ${number * 9}`);
+// console.log(`${number} * 10 = ${number * 10}`);
+
+// Write a program to perform all the arithmetic operations[except increment and decrement operators] of
+// javascript of any two numbers stored in the variables num1 and num2. Also, print the results to the console.
+
+let num1 = 10;
+let num2 = 8;
--- Project 15/object.js
+++ Project 15/object.js
@@ -1,11 +1,11 @@
-/*****************Singleton******************************************/
-// Object.create
-// const tinderUser = new Object()
-// tinderUser.id =  "1278ss"
-// tinderUser.email = "mangalagan4@gmail.com"
-// tinderUser.age = 48;
-// console.log(tinderUser);
-/*****************Object Literals******************************************/
+// // /*****************Singleton******************************************/
+// // // Object.create
+// // const tinderUser = new Object()
+// // tinderUser.id =  "1278ss"
+// // tinderUser.email = "mangalagan4@gmail.com"
+// // tinderUser.age = 48;
+// // console.log(tinderUser);
+// // /*****************Object Literals******************************************/
 
 // const myUser = Symbol("Key")
 
@@ -19,67 +19,70 @@
 //     lastLoginDays: ["Manday", "Saturday"]
 // }
 // console.log(User);
-// console.log(User["email"]);
-// User.email = "mangalgagan4@gmail.com"
-// Object.freeze(User)
-// User.email = "mangalgagan6@gmail.com"
-// console.log(User);
+// console.log(typeof user.myUser);
+// // // console.log(User["email"]);
+// // // User.email = "mangalgagan4@gmail.com"
+// // // Object.freeze(User)
+// // // User.email = "mangalgagan6@gmail.com"
+// // // console.log(User);
 
-// User.One = function(){
-//     console.log(`Hi I am ${this.name}`);
-// }
-// console.log(User.One());
+// // // User.One = function(){
+// // //     console.log(`Hi I am ${this.name}`);
+// // // }
+// // // console.log(User.One());
 
-const tinderUser = {
-  email: "gagan09@gmail.com",
-  fullname: {
-    username: {
-      firstname: "gagan",
-      lastname: "mangal",
-    },
-  },
-};
-// console.log(tinderUser);
-// console.log(tinderUser.fullname.username);
+// // const tinderUser = {
+// //   email: "gagan09@gmail.com",
+// //   fullname: {
+// //     username: {
+// //       firstname: "gagan",
+// //       lastname: "mangal",
+// //     },
+// //   },
+// // };
+// // // console.log(tinderUser);
+// // // console.log(tinderUser.fullname.username);
 
-const obj1 = {1: "a", 2: "b"}
-const obj2 = {3: "c", 4: "d"}
+// // const obj1 = {1: "a", 2: "b"}
+// // const obj2 = {3: "c", 4: "d"}
 
-// const obj3 = {obj1,obj2}
-// const obj3 = Object.assign({}, obj1, obj2)
-// const obj3 = {...obj1, ...obj2}
-// console.log(obj3);
+// // // const obj3 = {obj1,obj2}
+// // // const obj3 = Object.assign({}, obj1, obj2)
+// // // const obj3 = {...obj1, ...obj2}
+// // // console.log(obj3);
 
 
 
-const User = [
-    {
-        id: 1,
-        email: 'h@gmail.com'
-    },
-    {
-        id: 2,
-        email: 'h@gmail.com'
-    },
-    {
-        id: 3,
-        email: 'hpp@gmail.com'
-    },
-    {
-        id: 4,
-        email: 'h@gmail.com'
-    },
-    {
-        id: 5,
-        email: 'h@gmail.com'
-    }
-]
+// // const User = [
+// //     {
+// //         id: 1,
+// //         email: 'h@gmail.com'
+// //     },
+// //     {
+// //         id: 2,
+// //         email: 'h@gmail.com'
+// //     },
+// //     {
+// //         id: 3,
+// //         email: 'hpp@gmail.com'
+// //     },
+// //     {
+// //         id: 4,
+// //         email: 'h@gmail.com'
+// //     },
+// //     {
+// //         id: 5,
+// //         email: 'h@gmail.com'
+// //     }
+// // ]
 
-// console.log(User);
-// console.log(User[2].email);
+// // // console.log(User);
+// // // console.log(User[2].email);
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+// // // console.log(Object.keys(tinderUser));
+// // // console.log(Object.values(tinderUser));
+// // // console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('userfullnmae'))
+// // console.log(tinderUser.hasOwnProperty('userfullnmae'))
+
+// /**************************************************************************************************************************************/
