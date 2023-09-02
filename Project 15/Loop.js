2e84ef4474075afa2aa893487b4a370fd43e18ea
Gaganfnr <mangalgagan4@gmail.com>
2023-09-02 14:08:02 +0530

    ...

--- Project 15/Loop.js
+++ Project 15/Loop.js
@@ -1,85 +1,103 @@
-// // const myArray = [1, 2, 3, 4, 5, 6];
-// /*******************************FOR LOOP*******************************/
-// // for (let i = 0; i < myArray.length; i++) {
-// //   const element = i;
-// //   //   console.log(element);
+// // // const myArray = [1, 2, 3, 4, 5, 6];
+// // /*******************************FOR LOOP*******************************/
+// // // for (let i = 0; i < myArray.length; i++) {
+// // //   const element = i;
+// // //   //   console.log(element);
+// // // }
+
+// // /*******************************FOROF LOOP**************************/
+
+// // for (const num of myArray) {
+// //   console.log(num);
 // // }
 
-// /*******************************FOROF LOOP**************************/
+// // // const arr = "Gagan Mangal";
+// // // for (const string of arr) {
+// // //   if (string == " ") {
+// // //     continue;
+// // //   } else {
+// // //     // console.log(`string: ${string}`);
+// // //   }
+// // // }
 
-// for (const num of myArray) {
-//   console.log(num);
-// }
+// // /***************************************MAPS****************************************/
 
-// // const arr = "Gagan Mangal";
-// // for (const string of arr) {
-// //   if (string == " ") {
-// //     continue;
-// //   } else {
-// //     // console.log(`string: ${string}`);
-// //   }
+// // const mapObject = new Map();
+// // mapObject.set("Nmae", "Gagan");
+// // mapObject.set("Age", "20");
+// // mapObject.set("Id", "102A");
+// // mapObject.set("city", "udaipur");
+// // mapObject.set("class", "second");
+
+// // // console.log(mapObject);
+
+// // for (const [key, value] of mapObject) {
+// //   // console.log(`keys: ${key} value: ${value}`);
+// // }
+// // /***********************************OBJECT*************************************************/
+
+// // const oBject = {
+// //   name: "gagan mangal",
+// //   age: 20,
+// //   id: "102K",
+// //   class: "second",
+// // };
+
+// // // for (const Bject of oBject) {
+// // //     console.log(Bject);
+// // // }
+// // /********************************************FORIN LOOP*****************************/
+
+// // for (const key in oBject) {
+// // //   console.log(`keys: ${key} value: ${oBject[key]}`);
 // // }
 
-// /***************************************MAPS****************************************/
-
-// const mapObject = new Map();
-// mapObject.set("Nmae", "Gagan");
-// mapObject.set("Age", "20");
-// mapObject.set("Id", "102A");
-// mapObject.set("city", "udaipur");
-// mapObject.set("class", "second");
-
-// // console.log(mapObject);
-
-// for (const [key, value] of mapObject) {
-//   // console.log(`keys: ${key} value: ${value}`);
-// }
-// /***********************************OBJECT*************************************************/
-
-// const oBject = {
-//   name: "gagan mangal",
-//   age: 20,
-//   id: "102K",
-//   class: "second",
-// };
-
-// // for (const Bject of oBject) {
-// //     console.log(Bject);
+// // const arr = ['gagan','rohit','deven','umesh','gargi']
+// // for (const key in arr) {
+// //     console.log(`${key}: ${arr[key]}`);
 // // }
-// /********************************************FORIN LOOP*****************************/
 
-// for (const key in oBject) {
-// //   console.log(`keys: ${key} value: ${oBject[key]}`);
-// }
+// // /*********************************************FOREACH LOOP*************************************/
 
-// const arr = ['gagan','rohit','deven','umesh','gargi']
-// for (const key in arr) {
-//     console.log(`${key}: ${arr[key]}`);
-// }
+// const nAme = ["gagan", "rohit", "deven", "umesh", "gargi"];
 
-// /*********************************************FOREACH LOOP*************************************/
+// nAme.forEach((element, index, nAme) => {
+// //   console.log(element, index, nAme);
+// });
 
-const nAme = ["gagan", "rohit", "deven", "umesh", "gargi"];
+// const nameObject = [
+//   {
+//     first_name: "gagan",
+//     last_name: "mangal",
+//   },
+//   {
+//     first_name: "rohit",
+//     last_name: "lohar",
+//   },
+//   {
+//     first_name: "deven",
+//     last_name: "jain",
+//   },
+// ];
 
-nAme.forEach((element, index, nAme) => {
-//   console.log(element, index, nAme);
+// nameObject.forEach((element) => {
+//     console.log(`firstname: ${element.first_name}`);
+// })
+
+/*****************************************************FILTER***********************************************************/
+
+const num = [1, 2, 3, 4, 5, 6, 8, 9, 10];
+
+// let bbb= num.forEach((element) => {
+//   const hhh = element == 5;
+//   return hhh;
+// });
+// console.log(bbb);
+
+/*  foreach never return any value   so we use filter and map     */
+
+let newNumber = num.filter((num) => {
+  return num > 5;
 });
 
-const nameObject = [
-  {
-    first_name: "gagan",
-    last_name: "mangal",
-  },
-  {
-    first_name: "rohit",
-    last_name: "lohar",
-  },
-  {
-    first_name: "deven",
-    last_name: "jain",
-  },
-];
-
-nameObject.forEach((element) => {
-    console.log(`firstname: ${element.first_name}`);
-})
+console.log(newNumber);
