318936eb7c3ac63f9b89a3f258633920327b3591
Gaganfnr <mangalgagan4@gmail.com>
2023-09-04 10:02:04 +0530

    ...

---
+++ Project 16/01.js
@@ -0,0 +1,30 @@
+//   document.getElementById("title");
+//   document.getElementById("title").id;
+//   document.getElementById("title").className;
+//   document.getElementById("title").getAttribute("id");
+//   document.getElementById("title").getAttribute("className");
+//   document.getElementById("ll").setAttribute("class", "oo");
+
+//   const title = document.getElementById("title");
+//   title.style.backgroundColor = "green";
+//   title.style.borderRadius = "15px";
+
+//   const ll = document.getElementById("ll");
+//   ll.innerText;
+//   ll.textContent;
+//   ll.innerHTML;
+
+//   document.querySelector("#ll");
+//   document.querySelector(".heding");
+
+//   document.querySelector("ul");
+//   const myli = document.querySelector("li");
+//   myli.style.color = "red";
+//   myli.textContent = "jain";
+
+// const mylist = document.querySelectorAll("li");
+// mylist.forEach((li) => {
+//     if (li.textContent == 'one'){
+//         li.style.color = 'green';
+//     } 
+// } );
