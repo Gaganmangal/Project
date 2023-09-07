bf5998a7808f34d5cdf080c834c913c0c088c68a
Gaganfnr <mangalgagan4@gmail.com>
2023-09-07 19:51:34 +0530

    ...

--- Project 19/color.js
+++ Project 19/color.js
@@ -1,20 +1,23 @@
 const button_1 = document.querySelector(".button_1");
 const body = document.querySelector("body");
-
-button_1.addEventListener("click", () => {
-  let text = button_1.innerHTML;
+button_1.addEventListener("click", (e) => {
+  let text = e.target.id;
+  // console.log(text);
   changecolor(text);
 });
 
 function changecolor(text) {
-  if (text == '<i class="fa-regular fa-moon"></i> Dark Mode') {
+  if (text == "Dark") {
+    // alert("jj");
     button_1.innerHTML = '<i class="fa-regular fa-sun"></i> Light Mode';
     body.style.backgroundColor = "#212121";
     body.style.transition = "All 2s";
+    button_1.id = "Light";
   }
-  if (text != '<i class="fa-regular fa-moon"></i> Dark Mode') {
+  if (text == "Light") {
     button_1.innerHTML = '<i class="fa-regular fa-moon"></i> Dark Mode';
     body.style.backgroundColor = "white";
     body.style.transition = "All 2s";
+    button_1.id = "Dark";
   }
 }
