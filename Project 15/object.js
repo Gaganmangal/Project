ef8ce7e4c01365992a670076ca30b8ffe99120af
Gaganfnr <mangalgagan4@gmail.com>
2023-09-14 21:58:42 +0530

    ...

--- Project 15/object.js
+++ Project 15/object.js
@@ -7,19 +7,19 @@
 // // // console.log(tinderUser);
 // // // /*****************Object Literals******************************************/
 
-// // const myUser = Symbol("Key")
+const myUser = Symbol("Key")
 
-// // const User = {
-// //     name: "GaganMangal",
-// //     age: 20,
-// //     location: "Udaipur",
-// //     [myUser]: "Key_1",
-// //     email: "gaganmangal350@gmail.com",
-// //     isloggedIn: false,
-// //     lastLoginDays: ["Manday", "Saturday"]
-// // }
-// // console.log(User);
-// // console.log(typeof user.myUser);
+const user = {
+    name: "GaganMangal",
+    age: 20,
+    location: "Udaipur",
+    [myUser]: "Key_1",
+    email: "gaganmangal350@gmail.com",
+    isloggedIn: false,
+    lastLoginDays: ["Manday", "Saturday"]
+}
+// console.log(user);
+// console.log(user.isloggedIn);
 // // // // console.log(User["email"]);
 // // // // User.email = "mangalgagan4@gmail.com"
 // // // // Object.freeze(User)
@@ -31,17 +31,17 @@
 // // // // }
 // // // // console.log(User.One());
 
-// // // const tinderUser = {
-// // //   email: "gagan09@gmail.com",
-// // //   fullname: {
-// // //     username: {
-// // //       firstname: "gagan",
-// // //       lastname: "mangal",
-// // //     },
-// // //   },
-// // // };
+const tinderUser = {
+  email: "gagan09@gmail.com",
+  fullname: {
+    username: {
+      firstname: "gagan",
+      lastname: "mangal",
+    },
+  },
+};
 // // // // console.log(tinderUser);
-// // // // console.log(tinderUser.fullname.username);
+// console.log(tinderUser.fullname.username);
 
 // const obj1 = {1: "a", 2: "b"}
 // const obj2 = {3: "c", 4: "d"}
@@ -51,35 +51,35 @@
 // const obj3 = {...obj1, ...obj2}
 // console.log(obj3);
 
-// // // const User = [
-// // //     {
-// // //         id: 1,
-// // //         email: 'h@gmail.com'
-// // //     },
-// // //     {
-// // //         id: 2,
-// // //         email: 'h@gmail.com'
-// // //     },
-// // //     {
-// // //         id: 3,
-// // //         email: 'hpp@gmail.com'
-// // //     },
-// // //     {
-// // //         id: 4,
-// // //         email: 'h@gmail.com'
-// // //     },
-// // //     {
-// // //         id: 5,
-// // //         email: 'h@gmail.com'
-// // //     }
-// // // ]
+const User = [
+    {
+        id: 1,
+        email: 'h@gmail.com'
+    },
+    {
+        id: 2,
+        email: 'h@gmail.com'
+    },
+    {
+        id: 3,
+        email: 'hpp@gmail.com'
+    },
+    {
+        id: 4,
+        email: 'h@gmail.com'
+    },
+    {
+        id: 5,
+        email: 'h@gmail.com'
+    }
+]
 
 // // // // console.log(User);
-// // // // console.log(User[2].email);
+// console.log(User[2].email);
 
-// // // // console.log(Object.keys(tinderUser));
-// // // // console.log(Object.values(tinderUser));
-// // // // console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
 // // // console.log(tinderUser.hasOwnProperty('userfullnmae'))
 
---
+++ Project 26/Password Validator/index.js
@@ -0,0 +1,13 @@
+// const password = document.querySelector("#Password");
+// const button = document.querySelector("button");
+// const eye = document.querySelector("span");
+// eye.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   if (password.type === "password") {
+//     password.type = "text";
+//     eye.innerHTML = `<i class="fa-solid fa-eye-slash"></i>`;
+//   } else {
+//     password.type = "password";
+//     eye.innerHTML = `<i class="fa-solid fa-eye-slash"></i>`;
+//   }
+// });
