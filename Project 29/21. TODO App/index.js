0ee6d33d265e2b1daabf85cee1d6070e7e9a28c2
Gaganfnr <mangalgagan4@gmail.com>
2023-10-03 18:55:22 +0530

    ...

--- Project 29/21. TODO App/index.js
+++ Project 29/21. TODO App/index.js
@@ -2,8 +2,9 @@ const text = document.getElementById("text");
 const button = document.getElementById("button");
 const list = document.getElementById("list");
 
-let deletebutton = button.addEventListener("click", () => {
-  if (text.value !== " ") {
+button.addEventListener("click", () => {
+  if (text.value !== "") 
+  {
     const li = document.createElement("li");
     const button = document.createElement("button");
     button.classList.add("delete-button");
@@ -12,28 +13,14 @@ let deletebutton = button.addEventListener("click", () => {
     li.textContent = text.value;
     list.appendChild(li);
     list.appendChild(button);
-    text.value = " ";
-  }
-//   return button;
-});
-console.log( deletebutton);
-// deletebutton.addEventListener("click", () => {
-//     list.removeChild(li);
-//     list.removeChild(button);
-// });
+    text.value = "";
+    console.log(list);
 
-// console.log(text.value);
-// function check(e) {
-// text.addEventListener('input',()=>{
-//     console.log(input);
-//     if (text.value === " ") {
-//         e.disabled = true;
-//         console.log(e.disabled);
-//       } else if (text.value !== " ") {
-//         e.disabled = false;
-//         console.log(e.disabled);
-//         console.log(" ");
-//       }
-// })
-// }
-// console.log(check(button));
+    button.addEventListener("click", () => {
+      list.removeChild(li);
+      list.removeChild(button);
+      console.log(list);
+    });
+  }
+});
+
