1ddf793a0f377e35447f7497869127753137d11c
Gaganfnr <mangalgagan4@gmail.com>
2023-10-14 11:14:11 +0530

    ...

---
+++ Project 34/Working of JavaScript Assignment/script3.js
@@ -0,0 +1,8 @@
+const sum = addNumbers(10, 20);
+console.log(sum);
+
+function addNumbers(num1, num2) {
+  console.log(add);
+  var add = num1 + num2;
+  return add;
+}
---
+++ Project 34/Working of JavaScript Assignment/script4.js
@@ -0,0 +1,11 @@
+{
+  console.log(x);
+
+  var x = "Value Of X";
+  let y = "Value Of Y";
+  const z = "Value Of z";
+
+  console.log(x);
+  console.log(y);
+  console.log(z);
+}
